abc2c2b315b27bd0e9fc03df8d4b526864f3243a
proXDhiya <prox.dhiya@gmail.com>
2022-08-11 22:11:24 +0100

    fix issue routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { PathLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,7 @@ import { TodosComponent } from './components/todos/todos.component';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ { provide: LocationStrategy, useClass: PathLocationStrategy } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
